f7269fe6620e2a32c348dfaa7ec9a90e4ba08a75
MyMrMush <code@pilz-mail.de>
2024-02-02 15:29:49 +0100

    add auth0 authentication to recipes backend

---
+++ app/backend/recipes/src/main/java/com/juotava/recipes/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.juotava.recipes.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final Environment environment;
+
+    public SecurityConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/**").authenticated()
+        ).oauth2ResourceServer(oauth -> oauth
+                .jwt(jwt -> jwt.decoder(jwtDecoder()))
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        String issuerUri = environment.getProperty("spring.security.oauth2.resourceserver.jwt.issuer-uri");
+        assert issuerUri != null;
+        return JwtDecoders.fromIssuerLocation(issuerUri);
+    }
+
+}
