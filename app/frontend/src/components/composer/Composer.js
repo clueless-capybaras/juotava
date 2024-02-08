640877778e30c7dad2a2fef4194f24ac80aceec7
leonidehrler <leonid@ehrler-web.de>
2024-02-08 13:09:23 +0100

    implement provisional Composer UI

--- app/frontend/src/components/composer/Composer.js
+++ app/frontend/src/components/composer/Composer.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -8,6 +11,7 @@ import Row from 'react-bootstrap/Row';
 import ImageUploader from './ImageUploader';
 import IngredientList from './IngredientList';
 import PrepList from './PrepList';
+import TagField from './TagField';
 
 function Composer() {
 
@@ -21,9 +25,9 @@ function Composer() {
 
             <Row className="justify-content-center mb-3">
                 <Col xs="6" sm="6" md="6">
-                <FloatingLabel controlId="floatingTitle" label="Titel">
-                    <Form.Control placeholder="Titel" />
-                </FloatingLabel>
+                    <FloatingLabel controlId="floatingTitle" label="Titel">
+                        <Form.Control placeholder="Titel" />
+                    </FloatingLabel>
                 </Col>
                 <Col xs="2" sm="2" md="2">
                 </Col>
@@ -31,25 +35,27 @@ function Composer() {
 
             <Row className="justify-content-center mb-3">
                 <Col xs="6" sm="6" md="6">
-                <FloatingLabel controlId="floatingCategory" label="Kategorie">
-                    <Form.Select placeholder="Kategorie wählen...">
-                        <option>Kategorie wählen...</option>
-                        <option>Cocktail</option>
-                        <option>Kaffee</option>
-                        <option>Smoothie</option>
-                    </Form.Select>
-                </FloatingLabel>
+                    <FloatingLabel controlId="floatingCategory" label="Kategorie wählen...">
+                        <Form.Select placeholder="Kategorie wählen...">
+                            <option>Cocktail</option>
+                            <option>Kaffee</option>
+                            <option>Limonade</option>
+                            <option>Saft</option>
+                            <option>Smoothie</option>
+                            <option>Tee</option>
+                        </Form.Select>
+                    </FloatingLabel>
                 </Col>
                 <Col xs="2" sm="2" md="2">
-                <Form.Check type="checkbox" label="alkoholfrei" />
+                    <Form.Check type="checkbox" label="alkoholfrei" />
                 </Col>
             </Row>
 
             <Row className="justify-content-center mb-3">
                 <Col xs="8" sm="8" md="8">
-                <FloatingLabel label="Beschreibung" className="mb-3">
-                    <Form.Control as="textarea" placeholder="Beschreibung" style={{height: "5rem"}} />
-                </FloatingLabel>
+                    <FloatingLabel label="Beschreibung" className="mb-3">
+                        <Form.Control as="textarea" placeholder="Beschreibung" style={{height: "5rem"}} />
+                    </FloatingLabel>
                 </Col>
             </Row>
         </Container>
@@ -57,8 +63,8 @@ function Composer() {
         <Container className="mb-5">
             <h3 className="text-center">Zutatenliste</h3>
             <Row className="justify-content-center mb-3">
-                <Col xs="8" sm="8" md="8">
-                <IngredientList />
+                <Col>
+                    <IngredientList />
                 </Col>
             </Row>
         </Container>
@@ -66,8 +72,16 @@ function Composer() {
         <Container className="mb-5">
             <h3 className="text-center">Zubereitung</h3>
             <Row className="justify-content-center mb-3">
-                <Col xs="8" sm="8" md="8">
-                <PrepList />
+                <Col>
+                 <PrepList />
+                </Col>
+            </Row>
+        </Container>
+
+        <Container className="text-center mb-5">
+            <Row className="justify-content-center mb-3">
+                <Col>
+                    <TagField />
                 </Col>
             </Row>
         </Container>
@@ -75,8 +89,8 @@ function Composer() {
         <Container className="text-center mb-5">
             <Row className="justify-content-center mb-3">
                 <Col xs="8" sm="8" md="8">
-                <Button variant="primary" className="me-1">Veröffentlichen</Button>
-                <Button variant="secondary">Entwurf speichern</Button>
+                    <Button variant="primary" className="me-1">Veröffentlichen</Button>
+                    <Button variant="secondary">Entwurf speichern</Button>
                 </Col>
             </Row>
         </Container>
