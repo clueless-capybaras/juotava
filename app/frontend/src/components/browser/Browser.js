07d5a9ec638d38671ca45be2155b43efc35a15d6
Vanessa <vanni.limberger@gmail.com>
2024-02-20 19:47:10 +0100

    Initial

--- app/frontend/src/components/browser/Browser.js
+++ app/frontend/src/components/browser/Browser.js
@@ -2,12 +2,6 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 
 function Browser(props) {
-    let style = { width: 'auto', cursor: 'pointer', padding: '0' };
-    if(props.disabled) {
-        style = { width: 'auto', cursor: 'not-allowed', padding: '0', opacity: '0.5' };
-    }
-    const navigate = useNavigate();
-    const cyClass = (props.cyClass !== undefined) ? " "+props.cyClass : "";
     return (
         <h1>Browser page</h1>
     );
