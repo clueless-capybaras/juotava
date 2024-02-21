2fa21d8bbb2307fc9431f59d5e1e05eab11577ba
leonidehrler <leonid@ehrler-web.de>
2024-02-21 09:59:48 +0100

    set up structure for settings page

--- app/frontend/src/components/settings/about/About.js
+++ app/frontend/src/components/settings/about/About.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function About(props) {
-    let style = { width: 'auto', cursor: 'pointer', padding: '0' };
-    if(props.disabled) {
-        style = { width: 'auto', cursor: 'not-allowed', padding: '0', opacity: '0.5' };
-    }
-    const navigate = useNavigate();
-    const cyClass = (props.cyClass !== undefined) ? " "+props.cyClass : "";
+    
     return (
-        <h1>About page</h1>
+        <>
+        <h1>Über</h1>
+        
+        <Container>
+            <Row>
+                <Col>
+                    
+                </Col>
+            </Row>
+        </Container>
+        </>
+
     );
 }
 export default About;
