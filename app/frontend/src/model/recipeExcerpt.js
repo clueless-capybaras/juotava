8904fe991ff77b252ad18eeda1631e6dec0c324b
leonidehrler <leonid@ehrler-web.de>
2024-03-07 12:03:05 +0100

    add functionality to show recipe excerpts in browser

---
+++ app/frontend/src/model/recipeExcerpt.js
@@ -0,0 +1,10 @@
+export default class RecipeExcerpt {
+    constructor(title, category, nonAlcoholic, description, ingredients, image) {
+        this.title = title;
+        this.category = category;
+        this.nonAlcoholic = nonAlcoholic;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.image = image;
+    }
+}
