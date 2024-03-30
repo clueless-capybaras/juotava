2b9c7ced47581c62596f1571045f8dcf2145fd83
EhrlerL <leonid@ehrler-web.de>
2024-03-30 17:40:21 +0100

    use GenericBrowser in MyDrafts and MyRecipes

---
+++ app/frontend/src/components/settings/myrecipes/MyRecipes.js
@@ -0,0 +1,33 @@
+import { useContext, useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { AuthenticatedRequestWrapperContext } from '../../../App';
+import {baseUrlRecipes } from '../../../config/config';
+
+import { Button, Col, Row } from "react-bootstrap";
+
+import GenericBrowser from "../../general/GenericBrowser";
+
+function MyRecipes() {
+    const arw = useContext(AuthenticatedRequestWrapperContext);
+    const {user, isAuthenticated, getAccessTokenSilently} = useAuth0();
+
+    const [recipeExcerpts, setRecipeExcerpts] = useState([]);
+    const [loadRecipeExcerptsSuccess, setLoadRecipeExcerptsSuccess] = useState('');
+    useEffect(() => {
+        setLoadRecipeExcerptsSuccess('waiting');
+        arw.request({isAuthenticated, getAccessTokenSilently}, baseUrlRecipes, 'recipeexcerpt/mypublished', 'GET', undefined, setRecipeExcerpts, setLoadRecipeExcerptsSuccess, true);
+    }, []);
+
+
+
+    return (
+        <Row>
+            <Col>
+                <GenericBrowser recipeExcerpts={recipeExcerpts} loadRecipeExcerptsSuccess={loadRecipeExcerptsSuccess} />
+                <Button onClick={() => console.log(recipeExcerpts)}>Log</Button>
+            </Col>
+        </Row>
+    );
+}
+
+export default MyRecipes;
