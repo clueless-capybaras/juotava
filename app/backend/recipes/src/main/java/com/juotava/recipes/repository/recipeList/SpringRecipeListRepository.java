ae87525aacfe9c60468bc450f9499cbf43526714
Vanessa <vanni.limberger@gmail.com>
2024-04-09 17:45:22 +0200

    Add deleteRecipe method in RecipesService and frontend button to delete recipe

--- app/backend/recipes/src/main/java/com/juotava/recipes/repository/recipeList/SpringRecipeListRepository.java
+++ app/backend/recipes/src/main/java/com/juotava/recipes/repository/recipeList/SpringRecipeListRepository.java
@@ -2,6 +2,8 @@ package com.juotava.recipes.repository.recipeList;
 
 import com.juotava.recipes.model.RecipeList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +11,7 @@ import java.util.UUID;
 public interface SpringRecipeListRepository extends JpaRepository<RecipeList, UUID> {
     public List<RecipeList> findByCreatedBy(String auth0id);
     public RecipeList findByCreatedByAndTitle(String auth0id, String title);
+
+    @Query("SELECT rl FROM RecipeList rl JOIN rl.recipes r WHERE r.uuid = :recipeId")
+    List<RecipeList> findByRecipeId(@Param("recipeId") UUID recipeId);
 }
